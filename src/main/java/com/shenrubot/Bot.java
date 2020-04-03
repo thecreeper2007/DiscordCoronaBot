db04d400fc317a4f0ff41952736a4fee31ba9e28
chrisdesktop <zerves@gmail.com>
2020-04-03 15:49:15 -0700

    Initial

---
+++ src/main/java/com/shenrubot/Bot.java
@@ -0,0 +1,11 @@
+package com.shenrubot;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class Bot extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+
+    }
+}
