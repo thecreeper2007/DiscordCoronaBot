66d0cd6057263a9fe99b90573083eeadd6dd728f
Shenru <hushenru0@gmail.com>
2020-04-17 15:22:52 -0700

    :sparkles: got api key, test message.

--- src/main/java/com/shenrubot/Bot.java
+++ src/main/java/com/shenrubot/Bot.java
@@ -1,11 +1,26 @@
 package com.shenrubot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
 public class Bot extends ListenerAdapter {
-    public static void main(String[] args) throws LoginException {
+    public static String key = "NzAwODMwMTI5MjY5NjM3MjMw.XpopHA.wlNTRwROa0_BonvKEvVd3AWICOY";
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDA jda = new JDABuilder(key).build();
 
+        jda.awaitReady();
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        if(event.isFromType(ChannelType.PRIVATE){
+            System.out.println("Message Recieved!");
+        }
     }
 }
