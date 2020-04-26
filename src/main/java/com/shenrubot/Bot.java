469b4fa1ab6d804bcafd665c2617061424210b26
Shenru <hushenru0@gmail.com>
2020-04-25 20:10:23 -0700

    :sparkle: Made improvements, added features and squashed some bugs too!

--- src/main/java/com/shenrubot/Bot.java
+++ src/main/java/com/shenrubot/Bot.java
@@ -1,4 +1,6 @@
 package com.shenrubot;
+//https://pastebin.com/03sVdXUa
+//jda
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -8,8 +10,13 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
+//util
+//net
+//exceptions
+
 public class Bot extends ListenerAdapter {
     public static String key = "NzAwODMwMTI5MjY5NjM3MjMw.XpopHA.wlNTRwROa0_BonvKEvVd3AWICOY";
+    public static String baseURL = "https://api.thevirustracker.com/free-api?countryTotal=";
     public static void main(String[] args) throws LoginException, InterruptedException {
         JDA jda = new JDABuilder(key).addEventListeners(new Bot()).build();
 
@@ -19,10 +26,15 @@ public class Bot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
-        System.out.println("Message Recieved!");
-        if (!event.getAuthor().isBot()) {
+        String req = baseURL;
+        System.out.println("Message pinged14!");
+        String msg = event.getMessage().getContentDisplay();
+        if (msg.startsWith("corona?")) {
+            String country = msg.substring(7, 9);
+            string req = baseURL + country;
             MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue();
+
+            channel.sendMessage("");
         }
     }
 }
